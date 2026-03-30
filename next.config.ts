4cd9b8bde9157477b481efe334d9e2a5a7d1a77d
Vinit <singhvinit435@gmail.com>
2026-03-30 11:12:26 +0530

    fix date-fns package and next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["onjoqklqssepgohmegrd.supabase.co"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "onjoqklqssepgohmegrd.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
   },
 };
 
